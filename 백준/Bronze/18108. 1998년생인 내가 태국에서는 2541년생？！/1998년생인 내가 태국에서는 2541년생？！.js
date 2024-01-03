bc75d75cad4d9f46fee25042213c8e902dc0ff07
ethandeveloper2 <102854465+ethandeveloper2@users.noreply.github.com>
2024-01-03 10:45:01 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 140 ms, Memory: 9720 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.js
@@ -0,0 +1,37 @@
+/* readline Module */
+
+// 문제 풀이
+function solution(input) {
+    const diff = 2541 - 1998;
+    
+	// 답변 출력
+    console.log(input - diff);
+
+}
+
+
+
+/* readline Module */
+const readline = require("readline");
+const rl = readline.createInterface({
+
+	input: process.stdin,
+    output: process.stdout,
+
+});
+
+ 
+let input;
+
+rl.on("line", function (line) {
+    
+    input = line;           // 입력받은 문자열, line
+    input = parseInt(line);
+    rl.close();             // 입력 종료
+
+}).on("close", function () {
+
+    solution(input); // 문제 풀이 함수 호출
+    process.exit();  // 프로세스 종료
+
+});
