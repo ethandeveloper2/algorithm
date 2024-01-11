c2a8a08d2e53281155964c61b856198aea204078
ethandeveloper2 <102854465+ethandeveloper2@users.noreply.github.com>
2024-01-12 03:23:15 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 144 ms, Memory: 9432 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.js
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.js
@@ -1,28 +1,44 @@
+/* readline Module */
+
+// 문제 풀이
+function solution(input) {
+    const a = parseInt(input[0]);
+    const b = parseInt(input[1]);
+    
+    if (a > b) {
+        console.log('>');
+    } else if (a < b) {
+        console.log('<');
+    } else {
+        console.log('==');
+    }
+}
+
+
+
+/* readline Module */
 const readline = require("readline");
-
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+
+    input: process.stdin,
+    output: process.stdout,
+    
 });
 
-let input = [];
 
-rl.on("line", (line) => {
-  input = line.split(" ").map((el) => parseInt(el)); // 1 2 3 4
-  rl.close();
-});
+let input;
+let list = [];
 
-rl.on("close", () => {
-  const inputs = [];
-  input.forEach((el) => {
-    inputs.push(el);
-  });
-  if (inputs[0] < inputs[1]) {
-    console.log("<");
-  } else if (inputs[0] > inputs[1]) {
-    console.log(">");
-  } else {
-    console.log("==");
-  }
-  process.exit();
+rl.on("line", function (line) {
+
+    input = line; // 입력받은 문자열, line
+    rl.close();   // 입력 종료
+    
+}).on("close", function () {
+
+    // 형변환, 구분자(띄어쓰기)기준으로 배열에 삽입
+    list = input.split(' ').map((el) => Number(el));
+
+    solution(list); // 문제 풀이 함수 호출
+    process.exit(); // 프로세스 종료
 });
